df6a99d875cc2c4a6f7a038be8f0636b338d0a5a
Micaias <micaiasilvaop@gmail.com>
2025-04-27 00:16:54 -0300

    feat: category controller for end point /category which returns all existing categories

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { CategoryService } from './category.service';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  async findAll() {
+    return await this.categoryService.findAll()
+  }
 }
