02d76af7ac9379eb250ede5bd548049d1497bd5b
Micaias <micaiasilvaop@gmail.com>
2025-04-27 15:29:41 -0300

    feat: category service findAllOfManyCategories utility method

--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -1,11 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class CategoryService {
-    constructor(private databaseService: DatabaseService) {}
-    async findAll() {
-        return await this.databaseService.category.findMany({include: {parent: true}})
-    }
-}
+  constructor(private databaseService: DatabaseService) {}
+  async findAll() {
+    return await this.databaseService.category.findMany({
+      include: { parent: true },
+    });
+  }
 
+  async findAllOfManyCategories(categories: string[]) {
+    const foundCategories = await this.databaseService.category.findMany({
+      where: { name: { in: categories, mode: 'insensitive' } },
+    });
+
+    const foundMatchesRequested: boolean = foundCategories.every((item) =>
+      categories.includes(item.name),
+    );
+    if (!foundCategories) {
+      throw new NotFoundException("Some of requested categries were not found");
+    }
+
+    return foundCategories;
+  }
+}
