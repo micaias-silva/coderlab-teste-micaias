4e38041c95a6463776a663a9542581cb7995cea5
Micaias <micaiasilvaop@gmail.com>
2025-04-27 00:41:32 -0300

    feat: import product resource module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
 
 
 @Module({
-  imports: [DatabaseModule, CategoryModule],
+  imports: [DatabaseModule, CategoryModule, ProductModule],
 })
 export class AppModule {}
