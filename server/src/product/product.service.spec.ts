44898f27a78efd45bfd63bda1006ea92ce5addef
Micaias <micaiasilvaop@gmail.com>
2025-04-27 10:42:12 -0300

    feat: initial product creation test case

--- server/src/product/product.service.spec.ts
+++ server/src/product/product.service.spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductService } from './product.service';
-import { mockedValidProduct } from './mocks';
+import { mockedEmptyProduct, mockedNonexistentCategoryProduct, mockedValidProduct, mockedWrongTypedProduct } from './mocks';
 import { CreateProductDto } from './dto/create-product.dto';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('ProductService', () => {
   let productService: ProductService;
@@ -15,18 +16,43 @@ describe('ProductService', () => {
     productService = module.get<ProductService>(ProductService);
   });
 
+  // Support Functions
+  const checkCategoryMatches = (
+    productDto: CreateProductDto,
+    resultProduct,
+  ): boolean => {
+    return resultProduct.categories.every((item) => productDto.categories.includes(item.name))
+  };
+  const checkProductProperties = (resultProduct) => {
+    expect(resultProduct).toHaveProperty('id');
+    expect(resultProduct.name).toEqual(mockedValidProduct.name);
+    expect(resultProduct.qty).toEqual(mockedValidProduct.qty);
+    expect(resultProduct.price).toEqual(mockedValidProduct.price);
+    expect(resultProduct.photo).toEqual(mockedValidProduct.photo);
+    expect(checkCategoryMatches(mockedValidProduct, resultProduct)).toBe(true)
+  };
+
   it('should be defined', () => {
     expect(productService).toBeDefined();
   });
 
   describe('create', () => {
-    it('Should create a Product', () => {});
+    it('Should create a Product', async () => {
+      const createdProduct = await productService.create(mockedValidProduct);
+      checkProductProperties(createdProduct);
+    });
 
-    it('Should NOT create a Product with empty data', () => {});
+    it('Should NOT create a Product with empty data', async () => {
+      expect(await productService.create(mockedEmptyProduct)).toThrow(BadRequestException)
+    });
 
-    it('Should NOT create a Product with wrongly typed data', () => {});
+    it('Should NOT create a Product with wrongly typed data', async () => {
+      expect(await productService.create(mockedWrongTypedProduct)).toThrow(BadRequestException)
+    });
 
-    it('Should NOT create a Product with unexistent category', () => {});
+    it('Should NOT create a Product with unexistent category', async () => {
+      expect(await productService.create(mockedNonexistentCategoryProduct)).toThrow(NotFoundException)
+    });
   });
 
   describe('findOne', () => {
@@ -56,10 +82,10 @@ describe('ProductService', () => {
   });
 
   describe('remove', () => {
-    it("Should delete a product without exceptions", () => {})
+    it('Should delete a product without exceptions', () => {});
 
-    it("Should NOT delete a product that doesn't exist", () => {})
+    it("Should NOT delete a product that doesn't exist", () => {});
 
-    it("Should delete any relationships to its assigned categories", () => {})
+    it('Should delete any relationships to its assigned categories', () => {});
   });
 });
