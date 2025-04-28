1304d3a6a07b96e90e83f06a0e9451e9c9b8f4ef
Micaias <micaiasilvaop@gmail.com>
2025-04-28 19:32:11 -0300

    feat: category endpoint axios client

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -2,3 +2,5 @@ import axios from "axios";
 import { baseServerApiUrl } from "../config/api.config";
 
 export const productApi = axios.create({baseURL: baseServerApiUrl + "/product"})
+
+export const categoryApi = axios.create({baseURL: baseServerApiUrl + "/category"})
