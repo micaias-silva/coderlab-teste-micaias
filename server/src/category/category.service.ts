98409d68bf3a5d0e8392d229a5c5761c724b340f
Micaias <micaiasilvaop@gmail.com>
2025-04-27 22:08:58 -0300

    feat: eliminate duplicate items in category list from category dto

--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -4,22 +4,24 @@ import { DatabaseService } from 'src/database/database.service';
 @Injectable()
 export class CategoryService {
   constructor(private databaseService: DatabaseService) {}
-  async findAll() {
+  async findAll(page: number, itemCount: number) {
     return await this.databaseService.category.findMany({
       include: { parent: true },
     });
   }
 
   async findAllOfManyCategories(categories: string[]) {
+    const nonDuplicatedCategories = [...new Set(categories)]
+
     const foundCategories = await this.databaseService.category.findMany({
-      where: { name: { in: categories, mode: 'insensitive' } },
+      where: { name: { in: nonDuplicatedCategories, mode: 'insensitive' } },
     });
 
     const foundMatchesRequested: boolean =
-      foundCategories.length == categories.length &&
-      foundCategories.every((item) => categories.includes(item.name));
+      foundCategories.length == nonDuplicatedCategories.length &&
+      foundCategories.every((item) => nonDuplicatedCategories.includes(item.name));
     if (!foundMatchesRequested) {
-      throw new NotFoundException('Some of requested categries were not found');
+      throw new NotFoundException('Some of requested categories were not found');
     }
 
     return foundCategories;
