a793509715b21658d1ea6c472415c8be07170856
Micaias <micaiasilvaop@gmail.com>
2025-04-27 22:06:22 -0300

    feat: implemented query params page and itemCount to handle GET/category

--- server/src/product/product.controller.ts
+++ server/src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -13,8 +13,13 @@ export class ProductController {
   }
 
   @Get()
-  findAll() {
-    return this.productService.findAll();
+  findAll(
+    @Query('page')
+    page: string,
+    @Query('itemCount')
+    itemCount: string
+  ) {
+    return this.productService.findAll(+page || 1, +itemCount || 20);
   }
 
   @Get(':id')
