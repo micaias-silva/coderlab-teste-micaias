0835daf9a6d7440acf67d132feb2e576aa43ce66
Micaias <micaiasilvaop@gmail.com>
2025-04-28 19:28:02 -0300

    feat: yup schema for CreateProductForm

---
+++ client/src/components/Forms/CreateProductForm/form.schema.ts
@@ -0,0 +1,26 @@
+import * as yup from "yup";
+export const createProductSchema = yup.object({
+  name: yup.string().required("Product should be provided with a valid name"),
+
+  qty: yup
+    .number()
+    .typeError("Product qty should be an integer number")
+    .integer("Product qty should be an integer number")
+    .min(0, "Product qty should be at least 0")
+    .required("Product qty is required"),
+
+  price: yup
+    .number()
+    .typeError("Product price should be a number in format 0 or 0.00")
+    .min(0, "Product price should be at least 0")
+    .required("Product price is required"),
+
+  photo: yup
+    .string()
+    .url("Product should be provided with a valid url string")
+    .required("Photo URL is required"),
+
+  categories: yup.string(),
+});
+
+export default createProductSchema;
