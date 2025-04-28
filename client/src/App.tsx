16d82fd84afa24832b2873cf7bd0ac5d24f5d7be
Micaias <micaiasilvaop@gmail.com>
2025-04-28 15:40:53 -0300

    feat: react-router-dom implantation in main application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import ProductListContainer from './components/ProductListContainer'
 
 function App() {
 
 
   return (
     <>
+      <Routes>
+        <Route path='/' Component={ProductListContainer}/>
+      </Routes>
     </>
   )
 }
