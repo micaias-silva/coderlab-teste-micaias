537820554cfb313cfde33df05403de6f2e2cecf9
Micaias <micaiasilvaop@gmail.com>
2025-04-28 21:47:12 -0300

    feat: restructured updateProductSchema to have all properties optional

--- client/src/components/Forms/UpdateProductForm/form.schema.ts
+++ client/src/components/Forms/UpdateProductForm/form.schema.ts
@@ -1,25 +1,31 @@
 import * as yup from "yup";
-export const createProductSchema = yup.object({
-  name: yup.string().required("Product should be provided with a valid name"),
+
+const updateProductSchema = yup.object({
+  name: yup
+    .string()
+    .optional()
+    .trim()
+    .min(1, "Product should be provided with a valid name"),
 
   qty: yup
     .number()
     .typeError("Product qty should be an integer number")
     .integer("Product qty should be an integer number")
     .min(0, "Product qty should be at least 0")
+    .optional(),
 
   price: yup
     .number()
     .typeError("Product price should be a number in format 0 or 0.00")
     .min(0, "Product price should be at least 0")
-    .required("Product price is required"),
+    .optional(),
 
   photo: yup
     .string()
-    .url("Product should be provided with a valid url string")
-    .required("Photo URL is required"),
+    .url("Product should be provided with a valid URL string")
+    .optional(),
 
-  categories: yup.string(),
+  categories: yup.string().optional(),
 });
 
-export default createProductSchema;
+export default updateProductSchema;
