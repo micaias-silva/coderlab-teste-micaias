07959062685abbe353d7ea37c8826848a34bbd73
Micaias <micaiasilvaop@gmail.com>
2025-04-29 03:28:36 -0300

    feat: style sheet integration

--- frontend/src/pages/FormPage/FormPage.tsx
+++ frontend/src/pages/FormPage/FormPage.tsx
@@ -1,8 +1,9 @@
 import { PropsWithChildren } from "react"
 import { Link } from "react-router-dom"
+import "./style.css"
 
 const FormPage = ({children}: PropsWithChildren) => {
-    return <main>
+    return <main className="form-page">
         <Link to="/">Go back</Link>
         <div className="form-container">
             {children}
