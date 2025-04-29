7e234e90426dc3153826251613cb79be41be61d0
Micaias <micaiasilvaop@gmail.com>
2025-04-29 15:01:31 -0300

    feat: implemented product context api

--- frontend/src/components/ProductListContainer/index.tsx
+++ frontend/src/components/ProductListContainer/index.tsx
@@ -1,68 +1,58 @@
 import { productApi } from "../../utils/axios";
 import { GetProductNavigationResponse } from "../../interfaces/axios.interfaces";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import ProductList from "../ProductList";
-import {
-  useLocation,
-  useNavigate,
-  useSearchParams,
-} from "react-router-dom";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import { generateNumbersAround } from "../../utils/general.utils";
-import "./style.css"
+import "./style.css";
+import { ProductContext } from "../../providers/ProductContext";
 
 const ProductListContainer = () => {
-  const [response, setResponse] = useState<GetProductNavigationResponse>();
-
+  const { productResponse, updateQueryParams } = useContext(ProductContext);
   const [searchParams] = useSearchParams();
 
-  const [pageParam, setPageParam] = useState<string | null>(
-    searchParams.get("page") || "1"
-  );
+
   const [itemCountParam, setItemCountParam] = useState<string | null>(
     searchParams.get("itemCount") || "20"
   );
 
   const navigate = useNavigate();
   const location = useLocation();
-
   useEffect(() => {
-    const queryParams = `page=${pageParam || "1"}&itemCount=${
-      itemCountParam || "20"
-    }`;
-    productApi
-      .get(`/?${queryParams}`)
-      .then((res) => {
-        setResponse(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }, [pageParam]);
+    updateQueryParams({
+      page: searchParams.get("page") || "1",
+      itemCount: searchParams.get("itemCount") || "20",
+    });
+  }, [searchParams]);
 
   const HandlePaginate = (
     toPage: number,
     itemCount: number = Number(itemCountParam)
   ) => {
-    const queryParams = `page=${toPage}&itemCount=${itemCount}`;
-    setPageParam(toPage.toString());
+    updateQueryParams({
+      page: toPage.toString(),
+      itemCount: itemCount.toString(),
+    });
     setItemCountParam(itemCount.toString());
-    navigate(`${location.pathname}?${queryParams}`);
+    
+    const buildQueryParams = `page=${toPage}&itemCount=${itemCount}`;
+    navigate(`${location.pathname}?${buildQueryParams}`);
   };
 
   const calculateSurroudingPages = () => {
     return generateNumbersAround(
-      response!.currentPage,
+      productResponse!.currentPage,
       5,
       0,
-      response!.pageCount
+      productResponse!.pageCount
     );
   };
 
   return (
     <div>
-      {response && (
+      {productResponse && (
         <div className="product-list-container">
-          <ProductList products={response.navigation} />
+          <ProductList products={productResponse.navigation} />
           <div className="page-list-container">
             {calculateSurroudingPages().map((pageNumber) => {
               const href = `page=${pageNumber}&itemCount=${itemCountParam}`;
