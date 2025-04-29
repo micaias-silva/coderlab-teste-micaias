9d5425b1774982a5a7ce52e6b4dfc944e9e0a675
Micaias <micaiasilvaop@gmail.com>
2025-04-29 02:33:03 -0300

    feat: ProductListPage skeleton

---
+++ frontend/src/pages/ProductListPage/index.tsx
@@ -0,0 +1,12 @@
+import ProductListContainer from "../../components/ProductListContainer";
+
+const ProductListPage = () => {
+  return (
+    <div>
+      <header></header>
+      <ProductListContainer />
+    </div>
+  );
+};
+
+export default ProductListPage
