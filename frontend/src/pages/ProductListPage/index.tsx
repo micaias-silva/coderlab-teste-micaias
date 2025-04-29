01886a98b300b5beb15b64a2c7e06fbae3d6ccd4
Micaias <micaiasilvaop@gmail.com>
2025-04-29 02:47:19 -0300

    feat: ProductListPage stylization. Header received class .product-list-page-header; created .header-container to keep header content within desktop max-width

--- frontend/src/pages/ProductListPage/index.tsx
+++ frontend/src/pages/ProductListPage/index.tsx
@@ -1,14 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import ProductListContainer from "../../components/ProductListContainer";
+import "./style.css";
 
 const ProductListPage = () => {
   const navigate = useNavigate();
   return (
     <div>
-      <header>
-        <button onClick={() => navigate("/product/create")}>
-          Create new product
-        </button>
+      <header className="product-list-page-header">
+        <div className="header-container">
+          <button onClick={() => navigate("/product/create")}>
+            Create new product
+          </button>
+        </div>
       </header>
       <main>
         <ProductListContainer />
