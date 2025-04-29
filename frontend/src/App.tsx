014d709a3a6c023616b52b2857033f8b34b80547
Micaias <micaiasilvaop@gmail.com>
2025-04-29 03:15:57 -0300

    feat: implemented FormPage into app /product/update/:id and /product/create routes, taking their respective forms as a child

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,36 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import { CreateProductForm } from './components/Forms/CreateProductForm'
-import UpdateProductForm from './components/Forms/UpdateProductForm'
-import ProductPage from './pages/ProductPage'
-import ProductListPage from './pages/ProductListPage'
+import { Form, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { CreateProductForm } from "./components/Forms/CreateProductForm";
+import UpdateProductForm from "./components/Forms/UpdateProductForm";
+import ProductPage from "./pages/ProductPage";
+import ProductListPage from "./pages/ProductListPage";
+import FormPage from "./pages/FormPage/FormPage";
 
 function App() {
-
-
   return (
     <>
       <Routes>
-        <Route path='/' Component={ProductListPage}/>
-        <Route path='/product/create' Component={CreateProductForm}/> 
-        <Route path='/product/update/:id' Component={UpdateProductForm}/>
-        <Route path='/product/:id' Component={ProductPage}/>
-
+        <Route path="/" Component={ProductListPage} />
+        <Route
+          path="/product/create"
+          element={
+            <FormPage>
+              <CreateProductForm />
+            </FormPage>
+          }
+        />
+        <Route
+          path="/product/update/:id"
+          element={
+            <FormPage>
+              <UpdateProductForm />
+            </FormPage>
+          }
+        />
+        <Route path="/product/:id" Component={ProductPage} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
