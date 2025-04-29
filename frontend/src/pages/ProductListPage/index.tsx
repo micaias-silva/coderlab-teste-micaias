ba86033c4bcef8f9bf5c107e32fcaaec2323c177
Micaias <micaiasilvaop@gmail.com>
2025-04-29 02:34:26 -0300

    feat: create product button leading to /product/create

--- frontend/src/pages/ProductListPage/index.tsx
+++ frontend/src/pages/ProductListPage/index.tsx
@@ -1,12 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import ProductListContainer from "../../components/ProductListContainer";
 
 const ProductListPage = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      <header></header>
-      <ProductListContainer />
+      <header>
+        <button onClick={() => navigate("/product/create")}>
+          Create new product
+        </button>
+      </header>
+      <main>
+        <ProductListContainer />
+      </main>
     </div>
   );
 };
 
-export default ProductListPage
+export default ProductListPage;
