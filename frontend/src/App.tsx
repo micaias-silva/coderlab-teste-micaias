75ec4b71928c4e108207207009c50b5368576ff5
Micaias <micaiasilvaop@gmail.com>
2025-04-29 02:48:09 -0300

    feat: replaced ProductListContainter by ProductListPage in '/' app route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import ProductListContainer from './components/ProductListContainer'
 import { CreateProductForm } from './components/Forms/CreateProductForm'
 import UpdateProductForm from './components/Forms/UpdateProductForm'
 import ProductPage from './pages/ProductPage'
+import ProductListPage from './pages/ProductListPage'
 
 function App() {
 
@@ -11,10 +11,11 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' Component={ProductListContainer}/>
+        <Route path='/' Component={ProductListPage}/>
         <Route path='/product/create' Component={CreateProductForm}/> 
         <Route path='/product/update/:id' Component={UpdateProductForm}/>
         <Route path='/product/:id' Component={ProductPage}/>
+
       </Routes>
     </>
   )
